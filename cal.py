6bc31e3a26bbc3d889f200078acc9ad106f860ad
tsuyoshi <nakasone@std.it-college.ac.jp>
2016-07-19 16:09:56 +0900

    カレンダー表示機能の実装

--- cal.py
+++ cal.py
@@ -28,6 +28,39 @@ def cal(year, month=1, day=1):
     return (ee - ff + 1) % 7
 
 
-# 西暦を入力してもらい、その年のカレンダーを表示
+def is_leap_year(year):
+    if year % 400 == 0:
+        return True
+    if year % 100 == 0:
+        return False
+    if year % 4 == 0:
+        return True
+    return False
 
-# year = int(input('西暦(4桁)を入力してください: '))
+
+# 西暦を入力してもらい、その年のカレンダーを表示
+y = int(input('西暦(4桁)を入力してください: '))
+
+leap = is_leap_year(y)
+
+for m in range(1, 12 + 1):
+    print(m, '月')
+    print("Sun Mon Tue Wed Thu Fri Sat")
+    first = cal(y, m)
+    w = first
+    for d in range(1, 31 + 1):
+        if m == 2 and (not leap and d > 28 or leap and d > 29):
+            break
+        elif m in (4, 6, 9, 11) and d > 30:
+            break
+
+        if d == 1:
+            print('    ' * first, end='')
+        print(' ' * (1 - d // 10), d, end=' ')
+        w += 1
+        if w > 6:
+            print()
+            w = 0
+
+    print('\n')
+
